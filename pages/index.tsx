bb100298f1e7af74ba5f0a8d0522d67d94b0885d
Tanner Godarzi <tanner.godarzi@gmail.com>
2021-12-23 20:46:36 -0800

    Temporary bio

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,30 @@ export default function Index() {
 	return (
 		<>
 			<Head>
-				<title>Tanner Godarzi</title>
+				<title>Hello, I&rsquo;m Tanner</title>
 				<meta charSet="UTF-8" />
-				<meta name="description" content="Hi and hello — I'm Tanner" />
+				<meta name="title" content="Hello, I'm Tanner" />
+				<meta
+					name="description"
+					content="a Front End Engineer living in San Francisco. I&rsquo;m
+						currently at Notion telling the story of toolmaking for the
+						future."
+				/>
 				<meta name="keywords" content="Tanner Godarzi, Tanner, Notion, Dropbox, black ops" />
 				<meta name="author" content="Tanner Godarzi" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=true" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main className={styles.main}></main>
+			<main>
+				<article className={styles.bio}>
+					<p>
+						Hello, I&rsquo;m Tanner &mdash; a Front End Engineer &mdash; living in San Francisco. I&rsquo;m
+						currently at <a href="https://www.notion.so/">Notion</a> telling the story of toolmaking for the
+						future.
+					</p>
+				</article>
+			</main>
 		</>
 	);
 }
