6eb1205d71b6bd54c0ef4879524c66a31048be8f
Tanner Godarzi <tanner.godarzi@gmail.com>
2021-12-26 07:55:42 -0800

    Style experiments

--- components/block.tsx
+++ components/block.tsx
@@ -55,9 +55,26 @@ export const Block = ({ block }: PropsWithChildren<BlockProps>) => {
 			);
 		case "quote":
 			return (
-				<blockquote className={styles.quote}>
-					<Text value={value.text} />
-				</blockquote>
+				<>
+					<blockquote className={"quote"}>
+						<Text value={value.text} />
+					</blockquote>
+					<style jsx>{`
+						.quote {
+							font-size: 1.2rem;
+							font-weight: 700;
+							font-style: italic;
+							text-align: center;
+							quotes: "“" "”";
+						}
+						.quote::before {
+							content: open-quote;
+						}
+						.quote::after {
+							content: close-quote;
+						}
+					`}</style>
+				</>
 			);
 		case "divider":
 			return <hr key={id} className={styles.divider} />;
@@ -73,15 +90,35 @@ export const Block = ({ block }: PropsWithChildren<BlockProps>) => {
 				</figure>
 			);
 		case "image":
+			const { image } = block;
 			return (
-				<figure>
-					<Image src={block.image.file.url} width={1200} height={1200} alt={block.image.caption || ""} />
-					{block.image.caption.length > 0 && (
-						<figcaption>
-							<Text value={block.image.caption} />
-						</figcaption>
-					)}
-				</figure>
+				<>
+					<figure className="block-image">
+						<img src={image.file.url} alt={image.caption} />
+						{image.caption.length > 0 && (
+							<figcaption>
+								<Text value={image.caption} />
+							</figcaption>
+						)}
+					</figure>
+					<style jsx>{`
+						.block-image {
+							width: max(256px, 80%);
+							margin: auto;
+							display: block;
+						}
+						.block-image img {
+							display: block;
+							height: auto;
+							width: 100%;
+						}
+						.block-image figcaption {
+							margin-top: 0.25rem;
+							font-size: 0.8rem;
+							opacity: 0.8;
+						}
+					`}</style>
+				</>
 			);
 		case "embed":
 			return (
