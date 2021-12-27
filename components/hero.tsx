f7fe529b762fb953a939f81f61ce3028cd8fb78f
Tanner Godarzi <tanner.godarzi@gmail.com>
2021-12-27 10:08:31 -0800

    Hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import styles from "./hero.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 export const Hero = () => {
 	return (
 		<section className={styles.hero}>
 			<article>
 				<p className="omega">
-					<h1>{"Howdy, I’m Tanner — a cowboy on the internet — living in San Francisco, CA."} </h1>
-					{
-						"I’m A Front End Engineer telling the story of work tools at Notion. I love meeting new people over coffee."
-					}
+					{`Howdy, I’m Tanner — a cowboy on the internet `}
+					<span className={styles.poster}>
+						<Image src={"/cowboy.jpg"} alt={""} height={1117} width={1117} layout="fill" />
+					</span>
+					{` — living in San Francisco `}
+					<span className={styles.poster}>
+						<Image src={"/cowboy.jpg"} alt={""} height={1117} width={1117} layout="fill" />
+					</span>
+					{"I’m A Front End Engineer telling the story of the future of work at Notion. "}
+					<span className={styles.poster}>
+						<Image src={"/cowboy.jpg"} alt={""} height={1117} width={1117} layout="fill" />
+					</span>
+					{" I love meeting new people over coffee. "}
+					<span className={styles.poster}>
+						<Image src={"/cowboy.jpg"} alt={""} height={1117} width={1117} layout="fill" />
+					</span>
 				</p>
 			</article>
 		</section>
