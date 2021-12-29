f76afe302261cc27172042cc2c26fd156bf8c999
Tanner Godarzi <tanner.godarzi@gmail.com>
2021-12-28 17:04:10 -0800

    Images

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,6 +2,7 @@ import React, { PropsWithChildren, useEffect, useState } from "react";
 import styles from "./hero.module.css";
 import Image from "next/image";
 import classNames from "classnames";
+import Link from "next/link";
 
 interface PosterProps {}
 const Poster = ({ children }: PropsWithChildren<PosterProps>) => {
@@ -44,38 +45,49 @@ export const Hero = () => {
 		<section className={styles.hero}>
 			<article>
 				<p className={styles.hero_text}>
-					<span>{`Howdy, I’m Tanner — a real person on the internet — `}</span>
+					<span>{`Howdy, I’m Tanner — a real person on the internet.`}</span>
 					<Poster>
-						<Image src={"/clint-eastwood.gif"} alt={""} layout="fill" objectFit="cover" />
+						<Image
+							src={"https://media.giphy.com/media/xs7bOB7WyxnnQ9d0CS/giphy.gif"}
+							alt={""}
+							layout="fill"
+							objectFit="cover"
+						/>
 					</Poster>
 
-					<span>{` living in San Francisco. `}</span>
+					<span>{`I’m a Front End Engineer motivated by design thinking and story telling.`}</span>
 					<Poster>
-						<Image src={"/san-francisco.jpg"} alt={""} layout="fill" objectFit="cover" />
+						<Image
+							src={"https://media.giphy.com/media/sk6yL9EGVeAcE/giphy.gif"}
+							alt={""}
+							layout="fill"
+							objectFit="cover"
+						/>
 					</Poster>
-					<span>{" I’m a Front End Engineer at Notion on Brand Marketing. "}</span>
+					<span>{"Currently I’m at Notion on Brand Marketing telling the story of tools for work."}</span>
 					<Poster>
 						<Image src={"/notion-office.jpg"} alt={""} layout="fill" objectFit="cover" />
 					</Poster>
 					<span>
-						{" Story telling and engineering paired with design thinking are my passions when building. "}
+						{`San Francisco — specifically the delightful neighborhood of Cole Valley — is where I call home.`}
 					</span>
 					<Poster>
-						<Image src={"/the-good-the-bad-the-ugly.gif"} alt={""} layout="fill" objectFit="cover" />
+						<Image
+							src={"https://media.giphy.com/media/9ViAEnNgtuDUnVcfgi/giphy.gif"}
+							alt={""}
+							layout="fill"
+							objectFit="cover"
+						/>
 					</Poster>
-					<span>
-						{
-							" Integer consectetur blandit feugiat. Suspendisse in sem viverra, tempus enim vel, rhoncus mauris. "
-						}
-					</span>
+					<span>{"My favorite dish? Thanks for asking. I can’t deny the beauty of a Cacio e Pepe"}</span>
 					<Poster>
-						<Image src={"/the-good-the-bad-the-ugly.gif"} alt={""} layout="fill" objectFit="cover" />
+						<Image
+							src={"https://media.giphy.com/media/3o7bugOn9JDdvolbxu/giphy.gif"}
+							alt={""}
+							layout="fill"
+							objectFit="cover"
+						/>
 					</Poster>
-					<span>
-						{
-							" Integer consectetur blandit feugiat. Suspendisse in sem viverra, tempus enim vel, rhoncus mauris. "
-						}
-					</span>
 				</p>
 			</article>
 		</section>
