d39cab4431a5c0711bf34d76603998fc0e0dc4be
Tanner Godarzi <tanner.godarzi@gmail.com>
2022-02-05 10:20:36 -0800

    App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,50 @@ import "../styles/resets.css";
 import "../styles/variables.css";
 import "../styles/typography.css";
 
+import { useEffect } from "react";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import * as gtag from "../library/gtag";
+
 import Head from "next/head";
 
 function App({ Component, pageProps }) {
+	const router = useRouter();
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			gtag.pageview(url);
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
 	return (
 		<>
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			</Head>
 			<main>
+				{/* Global Site Tag (gtag.js) - Google Analytics */}
+				<Script
+					strategy="afterInteractive"
+					src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+				/>
+				<Script
+					id="gtag-init"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+
+						gtag('config', '${gtag.GA_TRACKING_ID}', {
+						page_path: window.location.pathname,
+						});
+					`,
+					}}
+				/>
 				<Component {...pageProps} />
 			</main>
 			<style jsx>{`
