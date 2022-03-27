f5f23ca16382889f06fc530f92527e03af5ab641
Tanner Godarzi <tanner.godarzi@gmail.com>
2022-03-27 12:54:14 -0700

    Bouncy component

--- components/bouncy.tsx
+++ components/bouncy.tsx
@@ -1,105 +1,114 @@
 // Forked from https://codepen.io/tannergodarzi/pen/oNXYWOr/e2009c1b26f7ee4034f678029e68c6ff
-import React, {useLayoutEffect, useEffect, useState } from "react";
+import React, { useLayoutEffect, useEffect, useState, useRef, useCallback } from "react";
 
 export default function Bouncy() {
-    const [position, setPosition] = React.useState({
-      x: 0,
-      y: 0
-    });
-    const [direction, setDirection] = React.useState({
-      x: "increment",
-      y: "increment"
-    });
-    const [bounds, setBounds] = React.useState({
-      x: 0,
-      y: 0
-    });
-    
-    const boundingBoxRef = React.useRef(null);
-  
-    const updatePosition = () => {
-      let xPos = position.x;
-      let yPos = position.y;
-      let xEnd = bounds.x;
-      let yEnd = bounds.y;
-      let xDirection = direction.x;
-      let yDirection = direction.y;
-      const start = 0;
-      
-  
-      // X Positioning
-      if (xDirection === "increment") {
-        xPos = xPos + 1;
-  
-        if (xPos > xEnd - 20) {
-          xDirection = "decrement";
-        }
-      } else if (direction.x === "decrement") {
-        xPos = xPos - 1;
-  
-        if (xPos <= start) {
-          xDirection = "increment";
-        }
-      }
-  
-      // Y Positioning
-      if (yDirection === "increment") {
-        yPos = yPos + 1;
-        if (yPos > yEnd - 20) {
-          yDirection = "decrement";
-        }
-      } else if (yDirection === "decrement") {
-        yPos = yPos - 1;
-        if (yPos <= start) {
-          yDirection = "increment";
-        }
-      }
-      setPosition({
-        x: xPos,
-        y: yPos
-      });
-      setDirection({
-        x: xDirection,
-        y: yDirection
-      });
-      window.requestAnimationFrame(updatePosition);
-    };
-  
-    useLayoutEffect(() => {
-      if (boundingBoxRef !== null) {
-        const yEnd = boundingBoxRef.getBoundingClientRect().height;
-        const xEnd = boundingBoxRef.getBoundingClientRect().width;
-        setBounds({
-            x: xEnd,
-            y: yEnd
-        });
-      }
-    }, [boundingBoxRef, setBounds]);
-  
-    useEffect(() => {
-      updatePosition();
-      return window.cancelAnimationFrame(updatePosition);
-    }, [updatePosition]);
-    
-    const styles = {
-            transform: `translate(${position.x}px, ${position.y}px)`
-    };
+	// Use useRef for mutable variables that we want to persist
+	// without triggering a re-render on their change
+	const boundingBoxRef = useRef<HTMLElement>(null);
+	const actorRef = useRef<HTMLDivElement>(null);
+	const windowAnimationRequestRef = useRef(null);
+	let boundsRef = useRef(null);
+	let directionRef = useRef({
+		x: "increment",
+		y: "increment",
+	});
+	let positionRef = useRef({
+		x: 0,
+		y: 0,
+	});
 
-    return (
-        <>
-      <div className="bounding-box" ref={boundingBoxRef}>
-        <div
-          className="actor"
-          style={styles}
-        />
-      </div>
-      <style jsx>{`.bounding-box {
-          display: block;
-          max-width: 720px;
-          width: 100%;
-          aspect-ratio: 1/1;
-      }`}</style>
-      </>
-    );
-  };
-  
+	useLayoutEffect(() => {
+		if (boundingBoxRef !== null) {
+			const yEnd = boundingBoxRef.current.getBoundingClientRect().height;
+			const xEnd = boundingBoxRef.current.getBoundingClientRect().width;
+			boundsRef.current = {
+				x: xEnd,
+				y: yEnd,
+			};
+		}
+		// TODO: Add Mutation observer
+	}, [boundsRef, boundingBoxRef]);
+
+	const updatePosition = () => {
+		if (boundsRef != undefined) {
+			let xPos = positionRef.current.x;
+			let yPos = positionRef.current.y;
+			let xEnd = boundsRef.current.x;
+			let yEnd = boundsRef.current.y;
+			let xDirection = directionRef.current.x;
+			let yDirection = directionRef.current.y;
+			const start = 0;
+
+			// X Positioning
+			if (xDirection === "increment") {
+				xPos = xPos + 1;
+
+				if (xPos > xEnd - 20) {
+					xDirection = "decrement";
+				}
+			} else if (xDirection === "decrement") {
+				xPos = xPos - 1;
+
+				if (xPos <= start) {
+					xDirection = "increment";
+				}
+			}
+
+			// Y Positioning
+			if (yDirection === "increment") {
+				yPos = yPos + 1;
+				if (yPos > yEnd - 20) {
+					yDirection = "decrement";
+				}
+			} else if (yDirection === "decrement") {
+				yPos = yPos - 1;
+				if (yPos <= start) {
+					yDirection = "increment";
+				}
+			}
+
+			positionRef.current = {
+				x: xPos,
+				y: yPos,
+			};
+			directionRef.current = {
+				x: xDirection,
+				y: yDirection,
+			};
+		}
+
+		actorRef.current.style.transform = `translate(${positionRef.current.x}px, ${positionRef.current.y}px)`;
+		windowAnimationRequestRef.current = window.requestAnimationFrame(updatePosition);
+	};
+
+	useEffect(() => {
+		windowAnimationRequestRef.current = requestAnimationFrame(updatePosition);
+		return () => cancelAnimationFrame(windowAnimationRequestRef.current);
+	}, []); // Make sure the effect runs only once
+
+	return (
+		<>
+			<section className="bounding-box" ref={boundingBoxRef}>
+				<div className="actor" ref={actorRef} />
+			</section>
+			<style jsx>{`
+				.bounding-box {
+					display: block;
+					max-width: 720px;
+					width: 100%;
+					height: auto;
+					aspect-ratio: 4/3;
+					border: 1px solid red;
+					overflow: hidden;
+				}
+
+				.actor {
+					width: 48px;
+					aspect-ratio: 1/1;
+					background: red;
+					will-change: transform;
+				}
+			`}</style>
+		</>
+	);
+}
