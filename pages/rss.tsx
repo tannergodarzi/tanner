f7c62b12629c87c703e008b51af3b4b804353787
Tanner Godarzi <tanner.godarzi@gmail.com>
2022-05-08 19:50:31 -0700

    reference

--- pages/rss.tsx
+++ pages/rss.tsx
@@ -31,6 +31,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const processedXml = `<?xml version="1.0" ?>
       <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
         <channel>
+            <atom:link href="https://tannergodarzi.com/rss" rel="self" type="application/rss+xml" />
             <title><![CDATA[Tanner Godarzi]]></title>
             <link>https://www.tannergodarzi.com/blog</link>
             <description>
