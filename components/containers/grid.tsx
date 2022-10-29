a591998681acc35465f83e1b3e08232bf97a873a
Tanner Godarzi <tanner.godarzi@gmail.com>
2022-10-29 11:51:40 -0700

    Grid

--- components/containers/grid.tsx
+++ components/containers/grid.tsx
@@ -8,85 +8,10 @@ import styles from "./grid.module.css";
 import Bio from "../bio";
 import Text from "../text";
 
-export default function Grid({ database }) {
+export default function Grid({ children }) {
 	return (
 		<>
-			<section className={styles.grid}>
-				{/*<GridElement column={2} columnSpan={4} row={1} rowSpan={6}>
-					<Image
-						src={"/tanner-foto.jpeg"}
-						layout="fill"
-						alt=""
-						objectFit="cover"
-						objectPosition={"center center"}
-					/>
-				</GridElement>
-				<GridElement column={7} columnSpan={5} row={1} rowSpan={6}>
-					<Bio />
-				</GridElement>
-
-				<GridElement column={12} columnSpan={4} row={1} rowSpan={4}>
-					<Image
-						src={"/tanner-polaroid.jpeg"}
-						layout="fill"
-						alt=""
-						objectFit="cover"
-						objectPosition={"center top"}
-					/>
-				</GridElement>*/}
-				<GridElement column={3} columnSpan={4} row={7} rowSpan={6}>
-					<iframe
-						src="https://open.spotify.com/embed/playlist/6CFbyGpSDA0eXAiPJQcqnG?theme=0"
-						width="100%"
-						height="100%"
-						frameBorder="0"
-						allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-					/>
-				</GridElement>
-				<GridElement column={10} columnSpan={6} row={7} rowSpan={7}>
-					<Image
-						src={"/NYC-2016-AIR-LAND-MATT_MURPHY-NOTE.jpg"}
-						layout="fill"
-						alt=""
-						objectFit="contain"
-						objectPosition={"top center"}
-					/>
-				</GridElement>
-				<GridElement column={2} columnSpan={6} row={15} rowSpan={8}>
-					<section className="frame container">
-						{database.map((entry) => (
-							<Entry entry={entry} key={entry.id} />
-						))}
-					</section>
-				</GridElement>
-				<GridElement column={10} columnSpan={3} row={18} rowSpan={3}>
-					<Link href={"/blog"}>
-						<a className="circle">{"Blog"}</a>
-					</Link>
-				</GridElement>
-				<GridElement column={12} columnSpan={3} row={14} rowSpan={2}>
-					<blockquote>{"Tanner is really cool"}</blockquote>
-				</GridElement>
-				{/*<GridElement column={10} columnSpan={4} row={14} rowSpan={3}>
-					<picture className="picture-frame">
-						<Image src={"/tanner-candid.jpg"} width={1000} height={675} alt="" />
-					</picture>
-						</GridElement>*/}
-				<GridElement column={10} columnSpan={4} row={22} rowSpan={6}>
-					<BioFollowUp />
-				</GridElement>
-				<GridElement column={2} columnSpan={8} row={26} rowSpan={6}>
-					<picture>
-						<Image
-							src={"/card.jpg"}
-							layout="fill"
-							alt=""
-							objectFit="contain"
-							objectPosition={"center top"}
-						/>
-					</picture>
-				</GridElement>
-			</section>
+			<section className={styles.grid}>{children}</section>
 			<style global jsx>{`
 				.circle {
 					color: var(--text-color);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import Bio from "../components/bio";
 import Grid from "../components/containers/grid";
+import Entry from "../components/entry";
 import Footer from "../components/footer";
+import GridElement from "../components/gridElement";
 import Hero from "../components/hero";
 import Navigation from "../components/navigation";
 import { getNotionDatabase } from "../helpers/notionHelpers";
@@ -36,7 +41,80 @@ export default function Index({ database }) {
 			<article>
 				<Hero />
 			</article>
-			<Grid database={database} />
+			<Grid>
+				<GridElement column={2} columnSpan={4} row={1} rowSpan={6}>
+					<Image
+						src={"/tanner-foto.jpeg"}
+						layout="fill"
+						alt=""
+						objectFit="cover"
+						objectPosition={"center center"}
+					/>
+				</GridElement>
+				<GridElement column={7} columnSpan={5} row={1} rowSpan={6}>
+					<Bio />
+				</GridElement>
+
+				<GridElement column={12} columnSpan={4} row={1} rowSpan={4}>
+					<Image
+						src={"/tanner-polaroid.jpeg"}
+						layout="fill"
+						alt=""
+						objectFit="cover"
+						objectPosition={"center top"}
+					/>
+				</GridElement>
+				<GridElement column={3} columnSpan={4} row={7} rowSpan={6}>
+					<iframe
+						src="https://open.spotify.com/embed/playlist/6CFbyGpSDA0eXAiPJQcqnG?theme=0"
+						width="100%"
+						height="100%"
+						frameBorder="0"
+						allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+					/>
+				</GridElement>
+				<GridElement column={10} columnSpan={6} row={7} rowSpan={7}>
+					<Image
+						src={"/NYC-2016-AIR-LAND-MATT_MURPHY-NOTE.jpg"}
+						layout="fill"
+						alt=""
+						objectFit="contain"
+						objectPosition={"top center"}
+					/>
+				</GridElement>
+				<GridElement column={2} columnSpan={6} row={15} rowSpan={8}>
+					<section className="frame container">
+						{database.map((entry) => (
+							<Entry entry={entry} key={entry.id} />
+						))}
+					</section>
+				</GridElement>
+				<GridElement column={10} columnSpan={3} row={18} rowSpan={3}>
+					<Link href={"/blog"}>
+						<a className="circle">{"Blog"}</a>
+					</Link>
+				</GridElement>
+				<GridElement column={12} columnSpan={3} row={14} rowSpan={2}>
+					<blockquote>{"Tanner is really cool"}</blockquote>
+				</GridElement>
+				{/*<GridElement column={10} columnSpan={4} row={14} rowSpan={3}>
+					<picture className="picture-frame">
+						<Image src={"/tanner-candid.jpg"} width={1000} height={675} alt="" />
+					</picture>
+						</GridElement>*/}
+				<GridElement column={10} columnSpan={4} row={22} rowSpan={6}></GridElement>
+				<GridElement column={2} columnSpan={8} row={26} rowSpan={6}>
+					<picture>
+						<Image
+							src={"/card.jpg"}
+							layout="fill"
+							alt=""
+							objectFit="contain"
+							objectPosition={"center top"}
+						/>
+					</picture>
+				</GridElement>
+			</Grid>
 			<Footer />
 		</>
 	);
