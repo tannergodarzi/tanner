a1ff21a1c180afb4b81dfc528ddc8c65c06cf31f
Tanner Godarzi <tanner.godarzi@gmail.com>
2022-12-28 13:14:33 -0700

    Wording

--- components/bio.tsx
+++ components/bio.tsx
@@ -10,9 +10,7 @@ export default function Bio() {
 			<p>
 				{`I love adventure and a good felt hat. During the day I help tell the story of future of work tools at Notion and by night I write about odd thoughts and conversations. Weekends I can be found meandering around San Francisco with a camera. Previously I worked at Dropbox, Square and design agency / freelance work. I also worked with heavy metal band Machine Head in a past time. I’m eternally optimistic about the future. If you are too, then`}
 			</p>
-			<Link href={"/about"}>
-				{"read more →"}
-			</Link>
+			<Link href={"/about"}>{"Read More →"}</Link>
 		</article>
 	);
 }
--- components/listicle.tsx
+++ components/listicle.tsx
@@ -11,17 +11,17 @@ export function Listicle() {
 				<dd>{"Deserts of the American Southwest"}</dd>
 				<dd>{"Every Ace Hotel"}</dd>
 				<dd>{"Point and shoot film cameras"}</dd>
+				<dd>{"Wearing a cool black leather jacket listening to Judas Priest"}</dd>
 				<dd>{"Land-use conspiracy theories"}</dd>
 				<dd>{"Danish Mid Century Modern"}</dd>
 				<dd>{"Multi-day Amtrak trips across the United States"}</dd>
 				<dd>{"Cocktail bars named after complete sentences"}</dd>
+				<dd>{"Dinner parties"}</dd>
 				<dd>
 					<Link href="https://twitter.com/ninlive/status/1183382334868983814">
 						{"This video of Trent Reznor starting a Nine Inch Nails set"}
 					</Link>
 				</dd>
-				<dd>{"Dinner parties"}</dd>
-				<dd>{"Wearing a cool black leather jacket listening to Judas Priest"}</dd>
 			</dl>
 		</article>
 	);
