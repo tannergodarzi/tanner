648c0665a5810c6854a158adc85e7cc27a73464d
Tanner Godarzi <tanner.godarzi@gmail.com>
2023-05-20 16:01:36 -0700

    Pic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,6 @@ import "../styles/typography.css";
 import styles from "./_app.module.css";
 
 import { useEffect } from "react";
-import Script from "next/script";
 import { useRouter } from "next/router";
 import * as gtag from "../library/gtag";
 import Head from "next/head";
@@ -28,29 +27,9 @@ function App({ Component, pageProps }) {
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			</Head>
 			<main className={styles.app}>
-				{/* Global Site Tag (gtag.js) - Google Analytics */}
-				<Script
-					strategy="afterInteractive"
-					src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-				/>
-				<Script
-					id="gtag-init"
-					strategy="afterInteractive"
-					dangerouslySetInnerHTML={{
-						__html: `
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){dataLayer.push(arguments);}
-						gtag('js', new Date());
-
-						gtag('config', '${gtag.GA_TRACKING_ID}', {
-						page_path: window.location.pathname,
-						});
-					`,
-					}}
-				/>
 				<Component {...pageProps} />
-				<Analytics />
 			</main>
+			<Analytics />
 		</>
 	);
 }
