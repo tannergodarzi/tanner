76af243c1e62456fe92e5c7428460719d8452180
Tanner Godarzi <tanner.godarzi@gmail.com>
2023-11-25 12:59:02 -0800

    Debug

--- components/block.tsx
+++ components/block.tsx
@@ -19,6 +19,7 @@ export default function Block({ block }: PropsWithChildren<BlockProps>) {
 							if (!column || !column.column) {
 								return;
 							}
+							console.log(column)
 							return (
 								<section
 									key={column.id}
