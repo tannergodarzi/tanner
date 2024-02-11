3cd31b9a9018317ab74109c77fed0091c1cae4d9
Tanner Godarzi <tanner.godarzi@gmail.com>
2024-02-10 17:21:20 -0800

    Space

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,6 +5,7 @@ import styles from "./footer.module.css";
 
 export default function Footer() {
 	const currentYear = new Date().getFullYear();
+	
 	return (
 		<>
 			<footer className={styles.footer}>
