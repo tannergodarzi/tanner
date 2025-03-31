bcb7e32b944ccb7276b6f6ebf08a778880dc300d
Tanner Godarzi <tanner.godarzi@gmail.com>
2025-03-31 11:51:20 -0700

    Misc fixes and new gif

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Poster from "./poster";
 
 import styles from "./hero.module.css";
+import Link from "next/link";
 
 export default function Hero() {
 	return (
@@ -15,29 +16,79 @@ export default function Hero() {
 					<p>
 						<span>{`— a real person on the internet.`}</span>
 						<Poster>
-							<Image src={"/gifs/skateboarder.gif"} alt={""} fill quality={50} />
+							<Image
+								src={"/gifs/skateboarder.gif"}
+								alt={""}
+								quality={50}
+								width={480}
+								height={360}
+								className={styles.posterImage}
+							/>
 						</Poster>
 						<span>{`I’m also a Front End Engineer motivated by design thinking and story telling.`}</span>
 						<Poster>
-							<Image src={"/gifs/reading.gif"} alt={""} fill quality={50} />
+							<Image
+								src={"/gifs/reading.gif"}
+								alt={""}
+								quality={50}
+								className={styles.posterImage}
+								width={384}
+								height={288}
+							/>
 						</Poster>
 						<span>{"Currently I’m at Notion on Brand Marketing telling the story of tools for work."}</span>
 						<Poster>
-							<Image src={"/gifs/mission-control.gif"} alt={""} fill quality={50} />
+							<Image
+								src={"/gifs/mission-control.gif"}
+								alt={""}
+								quality={50}
+								className={styles.posterImage}
+								width={480}
+								height={360}
+							/>
+						</Poster>
+						<span>
+							{"Occasionally I interview my pals at "}
+							<Link href="https://dinnerwithfriendsclub.com/">{"Dinner With Friends"}</Link>
+							{" about misadventures and chicken tenders"}
+						</span>
+						<Poster>
+							<Image
+								src={"/gifs/dinner-with-friends.gif"}
+								alt={""}
+								quality={50}
+								className={styles.posterImage}
+								width={900}
+								height={900}
+							/>
 						</Poster>
 						<span>
 							{`San Francisco — specifically the delightful neighborhood of Cole Valley — is where I call home.`}
 						</span>
 						<Poster>
-							<Image src={"/gifs/california.gif"} alt={""} fill quality={50} />
+							<Image
+								src={"/gifs/california.gif"}
+								alt={""}
+								quality={50}
+								className={styles.posterImage}
+								width={351}
+								height={263}
+							/>
 						</Poster>
 						<span>
 							{
-								"My favorite dish? Thanks for asking. I can’t deny the beauty of a Prosciutto di Parma Pie"
+								"My favorite dish? Thanks for asking. I can’t deny the beauty of a Prosciutto di Parma Pie."
 							}
 						</span>
 						<Poster>
-							<Image src={"/gifs/fruit-plate.gif"} alt={""} fill quality={50} />
+							<Image
+								src={"/gifs/fruit-plate.gif"}
+								alt={""}
+								quality={50}
+								className={styles.posterImage}
+								width={480}
+								height={320}
+							/>
 						</Poster>
 					</p>
 				</article>
